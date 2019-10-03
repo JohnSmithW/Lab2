85a15e0cfcc7324c47aee2b131272bb7cbf4a7c0
JohnSmith <salapin.danil1@mail.ru>
2019-10-03 21:03:11 +0100

    fixed design

--- task3-4/script.js
+++ task3-4/script.js
@@ -3,16 +3,7 @@ var comments = document.querySelectorAll("div.comments");
 let link = ["https://www.youtube.com/embed/M7XM597XO94", "https://www.youtube.com/embed/pU8-7BX9uxs", "https://www.youtube.com/embed/FknHjl7eQ6o", "https://www.youtube.com/embed/YApuEWtG30w"];
 
 function switchVideo(e){
-  if (myframe[e].className === "frame non-frame") {
-    
-    for (let i = 0; i < myframe.length; i++){
-      myframe[i].className = "frame non-frame";
-      myframe[i].src = link[i];
-      comments[i].className = "comments users-off";
-    }
-    myframe[e].className = "frame";
-    comments[e].className = "comments";
-  } 
+      myframe[0].src = link[e];  
 }
 
 
