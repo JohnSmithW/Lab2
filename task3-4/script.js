681b477de31a1b6f919761f9a891ffe1d5bcaa05
JohnSmith <salapin.danil1@mail.ru>
2019-10-04 21:49:45 +0100

    rewrited js

--- task3-4/script.js
+++ task3-4/script.js
@@ -1,28 +1,43 @@
-var myframe = document.querySelectorAll("iframe.frame");
-var comments = document.querySelectorAll("div.comments");
-let link = ["https://www.youtube.com/embed/M7XM597XO94", "https://www.youtube.com/embed/pU8-7BX9uxs", "https://www.youtube.com/embed/FknHjl7eQ6o", "https://www.youtube.com/embed/YApuEWtG30w"];
-
+var myframe = document.querySelectorAll('iframe.frame');
+let link = ['https://www.youtube.com/embed/M7XM597XO94', 'https://www.youtube.com/embed/pU8-7BX9uxs', 'https://www.youtube.com/embed/FknHjl7eQ6o', 'https://www.youtube.com/embed/YApuEWtG30w'];
+let a = 0;
+var currentComments = document.querySelectorAll('.comment-container');
+var videoBtn = document.querySelectorAll('.button button');
 function switchVideo(e){
-      myframe[0].src = link[e];  
+  if (link.length == videoBtn.length){
+      myframe[0].src = link[e];
+       a = e;  
+
+      for(let i = 0; i < currentComments.length; i++){
+        currentComments[i].classList.remove('visible');
+      }
+      currentComments[e].classList.add('visible');
+    }
+    else {alert('error')}
 }
 
+var
+form = document.forms[0],
+userName = form.elements.text,
+userCmnt = form.elements.comments,
+btn = document.querySelector('.submit'); 
+btn.addEventListener('click', () => {
+var newComment;  
+newComment = document.createElement('div');
+newComment.className = 'users';
+var nameTxt;
+var cmntTxt;
+nameTxt = document.createElement('h2');
+cmntTxt = document.createElement('p');
+nameTxt.innerHTML = userName.value;
+cmntTxt.innerHTML = userCmnt.value;
+document.querySelectorAll('.comment-container')[a].appendChild(newComment);
+newComment.appendChild(nameTxt);
+newComment.appendChild(cmntTxt);
 
-var btn = document.querySelector(".submit");
-  btn.addEventListener("click", () => {
-  var
-  user = document.querySelector(".user-text").value;
-  msg = document.querySelector(".textarea").value;
-  var
-  newBlock = document.createElement("div");
-  newBlock.className = "users";
-  title = document.createElement("h2");
-  com = document.createElement("p");
-  title.innerHTML = user;
-  com.innerHTML = msg;
-  document.querySelector(".comment-container").appendChild(newBlock);
-  newBlock.appendChild(title);
-  newBlock.appendChild(com);
 });
+
+
 
 
 
