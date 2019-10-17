b4cfd70f23ac30fd8b611339a3f2f60e4d290346
JohnSmith <salapin.danil1@mail.ru>
2019-10-17 17:40:24 +0100

    fixed some issues

--- task3-4/script.js
+++ task3-4/script.js
@@ -1,12 +1,12 @@
 var myframe = document.querySelectorAll('iframe.frame');
-let link = ['https://www.youtube.com/embed/M7XM597XO94', 'https://www.youtube.com/embed/pU8-7BX9uxs', 'https://www.youtube.com/embed/FknHjl7eQ6o', 'https://www.youtube.com/embed/YApuEWtG30w'];
-let a = 0;
+let links = ['https://www.youtube.com/embed/M7XM597XO94', 'https://www.youtube.com/embed/pU8-7BX9uxs', 'https://www.youtube.com/embed/FknHjl7eQ6o', 'https://www.youtube.com/embed/YApuEWtG30w'];
+let selectedVideoNum = 0;
 var currentComments = document.querySelectorAll('.comment-container');
 var videoBtn = document.querySelectorAll('.button button');
 function switchVideo(e){
-  if (link.length == videoBtn.length){
-      myframe[0].src = link[e];
-       a = e;  
+  if (links.length == videoBtn.length){
+      myframe[0].src = links[e];
+      selectedVideoNum = e;  
 
       for(let i = 0; i < currentComments.length; i++){
         currentComments[i].classList.remove('visible');
@@ -31,7 +31,7 @@ nameTxt = document.createElement('h2');
 cmntTxt = document.createElement('p');
 nameTxt.innerHTML = userName.value;
 cmntTxt.innerHTML = userCmnt.value;
-document.querySelectorAll('.comment-container')[a].appendChild(newComment);
+document.querySelectorAll('.comment-container')[selectedVideoNum].appendChild(newComment);
 newComment.appendChild(nameTxt);
 newComment.appendChild(cmntTxt);
 
